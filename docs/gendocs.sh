3bcfa37c1b71e26eb880046e116a8e27463a8949
Doumor <doumor@vk.com>
2024-09-08 12:19:41 +0300

    Change project scheme

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -1,1 +1,9 @@
-yfm -i ./diplodoc -o ./html
+docs_dir=./diplodoc
+for entry in "$docs_dir"/*
+do
+  project_name=$(echo $entry | tr "/" "\n" | tail -n 1)
+  mkdir html
+  project_path=./html/
+  project_path+=$project_name
+  yfm -i $entry -o $project_path
+done
